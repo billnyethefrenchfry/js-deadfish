5f44551b2037093e4f950d8d50f4c272c1aef1c5
billnyethefrenchfry <118934668+billnyethefrenchfry@users.noreply.github.com>
2022-11-23 16:05:21 -0500

    Add files via upload

---
+++ deadfish.js
@@ -0,0 +1,33 @@
+export function deadfish (code, ascii) {
+	let a = 0
+	let x = []
+	let c = code.split("")
+	if (typeof(code) === "string") {
+		for (let i = 0; i < code.length; i++) {
+			switch (c[i]) {
+				case 'i':
+					a++
+					break
+				case 'd':
+					a--
+					break
+				case 's':
+					a *= a
+					break
+				case 'o':
+					x.push(a)
+					break
+				default:
+					break
+			}
+			if (a == 256 || a < 0) {
+				a = 0
+			}
+		}
+	}
+	if (ascii === true) {
+		return String.fromCharCode.apply(null, x)
+	} else {
+		return x
+	}
+}
